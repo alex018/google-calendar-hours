e0c768cafc7951695029e3afbdf4c1141fbef3e6
Alex <alex@Alexs-MacBook-Air.local>
2026-02-14 08:24:44 +0800

    Updates

---
+++ precache-manifest.4261aed38bfa1f909d717ea1ce8393c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2744c8ca801ebe94125a95fe295a2b",
+    "url": "/google-calendar-hours/index.html"
+  },
+  {
+    "revision": "0bd70fa58b29af42c21c",
+    "url": "/google-calendar-hours/static/css/main.1ec6fd5d.chunk.css"
+  },
+  {
+    "revision": "fcc6886738fea8f87e02",
+    "url": "/google-calendar-hours/static/css/vendors~main.8f02cafe.chunk.css"
+  },
+  {
+    "revision": "0bd70fa58b29af42c21c",
+    "url": "/google-calendar-hours/static/js/main.1e1d09d9.chunk.js"
+  },
+  {
+    "revision": "96d1874c5e93abaca9d9",
+    "url": "/google-calendar-hours/static/js/runtime-main.46aeb567.js"
+  },
+  {
+    "revision": "fcc6886738fea8f87e02",
+    "url": "/google-calendar-hours/static/js/vendors~main.8bbd7832.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/google-calendar-hours/static/js/vendors~main.8bbd7832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a7cb04887751c08cba916885e3b5fd",
+    "url": "/google-calendar-hours/static/media/google_auth.75a7cb04.png"
+  }
+]);
